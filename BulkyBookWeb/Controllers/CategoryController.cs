b6a3aba7533ffd69dcd338d04c4aef94d0d8a3c5
PityoTibor <87979998+PityoTibor@users.noreply.github.com>
2022-03-10 16:23:06 +0100

    lattest

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -22,7 +22,59 @@ namespace BulkyBookWeb.Controllers
         public IActionResult Create()
         {
             
-            return View(objCategoryList();
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name"); 
+            }
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            { 
+                return NotFound();
+            }
+            var categoryFromDb = _db.categories.Find(id);
+            var categoryToDbFirst = _db.categories.FirstOrDefault(x => x.Id == id);
+            var categoryToDbSinge = _db.categories.SingleOrDefault(x => x.Id == id);
+
+            if (categoryToDbFirst == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
